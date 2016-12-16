56c9302e7825766bccb324591edc713a1c39a02f
degoldschmidt <goldschmidt.de@gmail.com>
2016-12-16 14:12:51 +0000

    More timing

--- tests/live.py
+++ tests/live.py
@@ -1,10 +1,10 @@
 # import the necessary packages
 from __future__ import print_function
 from imutils.video import VideoStream
-from imutils.video import FPS
 import argparse
 import imutils
 import cv2
+import time
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -16,8 +16,22 @@ args = vars(ap.parse_args())
 
 # grab a pointer to the video stream and initialize the FPS counter
 stream = cv2.VideoCapture(0)
+# Find OpenCV version
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+# With webcam get(CV_CAP_PROP_FPS) does not work.
+# Let's see for ourselves.
+if int(major_ver)  < 3 :
+	fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+	print "Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps)
+else :
+	fps = video.get(cv2.CAP_PROP_FPS)
+    print "Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps)
 
 # loop over some frames
+print "Capturing {0} frames".format(args["num_frames"])
+ # Start time
+start = time.time()
 while fps._numFrames < args["num_frames"]:
 	# grab the frame from the stream and resize it to have a maximum
 	# width of 400 pixels
@@ -29,6 +43,16 @@ while fps._numFrames < args["num_frames"]:
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
 
+# End time
+end = time.time()
+
+# Time elapsed
+seconds = end - start
+print "Time taken : {0} seconds".format(seconds)
+
+# Calculate frames per second
+fps  = num_frames / seconds;
+print "Estimated frames per second : {0}".format(fps);
 # do a bit of cleanup
 stream.release()
 cv2.destroyAllWindows()
