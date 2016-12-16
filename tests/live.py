fac24759d4088c70d7e5b2eb072d4dc2f84b4751
ADLER <ADLER@ADLER.com>
2016-12-16 13:59:21 +0000

    live demo

---
+++ tests/live.py
@@ -0,0 +1,55 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+# grab a pointer to the video stream and initialize the FPS counter
+stream = cv2.VideoCapture(0)
+
+# loop over some frames
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the stream and resize it to have a maximum
+	# width of 400 pixels
+	(grabbed, frame) = stream.read()
+	#frame = imutils.resize(frame, width=400)
+
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+# do a bit of cleanup
+stream.release()
+cv2.destroyAllWindows()
+
+# created a *threaded *video stream, allow the camera senor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+vs = VideoStream(src=0).start()
+
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	#frame = imutils.resize(frame, width=400)
+
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
