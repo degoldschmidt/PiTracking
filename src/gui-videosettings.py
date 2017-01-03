fe7091c0e2b85c4cd3e5528638ba26d061ff3a4a
degoldschmidt <goldschmidt.de@gmail.com>
2017-01-03 11:14:05 +0000

    Add sticky option

--- src/gui-videosettings.py
+++ src/gui-videosettings.py
@@ -31,7 +31,7 @@ class App():
         self.imageFrame = LabelFrame(self.root, text="Live Preview", width=self.mwidth, height=self.mheight, labelanchor='n')
         self.imageFrame.grid(row=0, rowspan=6, column=0, padx=10, pady=2)
         self.lmain = Label(self.imageFrame)
-        self.lmain.grid(row=0, rowspan=6, column=0)
+        self.lmain.grid(row=0, rowspan=6, sticky=W+E+N+S, column=0)
         self.write = self.initWriter()
         bri = Scale(self.root, from_=0, to=1, orient=HORIZONTAL, resolution=0.01, command=self.setBrightness, label = 'Brightness')
         bri.grid(row=0, column=1)
