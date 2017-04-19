4e93ca37a818629899dff337c71737681503a148
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 17:38:44 +0100

    variable freq

--- led/full_ir.py
+++ led/full_ir.py
@@ -3,6 +3,11 @@ import time
 
 print "Full IR LED Demo by Dennis Goldschmidt"
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--freq", type=int, default=1,
+	help="Frequency of blinking")
+args = vars(ap.parse_args())
+
 def blink(_led, freq=1):
     time.sleep(0.5/freq)
     _led.On(_led.IR1)
@@ -14,7 +19,7 @@ try:
     light.Reset();
 
     while True:
-        blink(light, 1.)
+        blink(light, args["freq"])
 
 except IOError as io:
     print ("I/O Error ({0})".format(io));
