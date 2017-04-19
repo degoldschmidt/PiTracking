d5793d4d5fd5fd5a6c971be3b5d12da9014b74a2
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 17:31:33 +0100

    test

--- led/full_ir.py
+++ led/full_ir.py
@@ -13,8 +13,14 @@ try:
     light = led.BrightPI(1)
     light.Reset();
 
-    for numbers in range(5):
-        blink(light)
+    time.sleep(0.5)
+    light.On(light.IR1)
+    time.sleep(0.5)
+    light.Off(light.IR1)
+    time.sleep(0.5)
+    light.On(light.IR1)
+    time.sleep(0.5)
+    light.Off(light.IR1)
 
 except IOError as io:
     print ("I/O Error ({0})".format(io));
