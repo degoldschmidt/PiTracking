812848656967a9896dca19d4ad6c2ab901db95b7
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 16:39:20 +0100

    test

--- led/full_ir.py
+++ led/full_ir.py
@@ -6,23 +6,8 @@ print "Full IR LED Demo by Dennis Goldschmidt"
 try:
     light = led.BrightPI(1)
     light.Reset();
-    light.IR_1_On();
-    time.sleep(0.5);
-    light.IR_2_On();
-    time.sleep(0.5);
-    light.IR_3_On();
-    time.sleep(0.5);
-    light.IR_4_On();
-    time.sleep(0.5);
-    light.IR_4_Off();
-    time.sleep(0.5);
-    light.IR_3_Off();
-    time.sleep(0.5);
-    light.IR_2_Off();
-    time.sleep(0.5);
-    light.IR_1_Off();
-    time.sleep(0.5);
-    light.Led_All_Off();
+    for times in range(100):
+        light.blink(light.IR1, 1)
 except IOError as io:
     print ("I/O Error ({0})".format(io));
     print " ex: sudo python demo.py"
--- led/led.py
+++ led/led.py
@@ -4,19 +4,20 @@
 # Beta Version 0.1                                  #
 
 import smbus
+import time
 
 
 class BrightPI:
     BrighPiAddress = 0x70;
     AddressControl = 0x00;
-    AddressIR1  = 0x01;
-    AddressLED1 = 0x02;
-    AddressIR2  = 0x03;
-    AddressLED2 = 0x04;
-    AddressLED3 = 0x05;
-    AddressIR3  = 0x06;
-    AddressLED4 = 0x07;
-    AddressIR4  = 0x08;
+    IR1  = 0x01;
+    LED1 = 0x02;
+    IR2  = 0x03;
+    LED2 = 0x04;
+    LED3 = 0x05;
+    IR3  = 0x06;
+    LED4 = 0x07;
+    IR4  = 0x08;
     AddressAllLed  = 0x09;
     maxBrightness = 0x32;
 
@@ -30,9 +31,15 @@ class BrightPI:
     def readState(self, address):
             return self.bus.read_byte_data(self.BrighPiAddress, address);
 
-    def IR_1_On(self):
+    def Blink(self, address, freq):
+        self.On(address)
+        time.sleep(1/freq)
+        self.Off(address)
+        time.sleep(1/freq)
+
+    def On(self, address):
         try:
-            mask = 0x01;
+            mask = address;
             result = self.readState(self.AddressControl) | mask;
             self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
         except IOError as io:
@@ -42,9 +49,9 @@ class BrightPI:
         except ValueError as ve:
             print ("Led 1 Activation ValueError : {0}".format(ve))
 
-    def IR_1_Off(self):
+    def Off(self, address):
         try:
-            mask = ~0x01;
+            mask = address;
             result = self.readState(self.AddressControl) & mask;
             self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
         except IOError as io:
@@ -55,259 +62,15 @@ class BrightPI:
             print ("Led 1 Deactivation ValueError : {0}".format(ve))
 
 
-    def IR_1_Brightness(self, level):
+    def setBrightness(self, address, level):
         try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressIR1,level);
+            self.bus.write_byte_data(self.BrighPiAddress, address, level);
         except IOError as io:
             print ("Led 1 Brightness IOError : {0}".format(io))
         except TypeError as te:
             print ("Led 1 Brightness TypeError : {0}".format(te))
         except ValueError as ve:
             print ("Led 1 Brightness ValueError : {0}".format(ve))
-
-    def IR_2_On(self):
-        try:
-            mask = 0x03;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Activation ValueError : {0}".format(ve))
-
-    def IR_2_Off(self):
-        try:
-            mask = ~0x03;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Deactivation ValueError : {0}".format(ve))
-
-
-    def IR_2_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressIR2,level);
-        except IOError as io:
-            print ("Led 1 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Brightness ValueError : {0}".format(ve))
-
-    def IR_3_On(self):
-        try:
-            mask = 0x06;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Activation ValueError : {0}".format(ve))
-
-    def IR_3_Off(self):
-        try:
-            mask = ~0x06;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Deactivation ValueError : {0}".format(ve))
-
-
-    def IR_3_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressIR3,level);
-        except IOError as io:
-            print ("Led 1 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Brightness ValueError : {0}".format(ve))
-
-    def IR_4_On(self):
-        try:
-            mask = 0x08;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Activation ValueError : {0}".format(ve))
-
-    def IR_4_Off(self):
-        try:
-            mask = ~0x08;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Deactivation ValueError : {0}".format(ve))
-
-
-    def IR_4_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressIR4,level);
-        except IOError as io:
-            print ("Led 1 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Brightness ValueError : {0}".format(ve))
-
-    def Led_1_On(self):
-        try:
-            mask = 0x02;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Activation ValueError : {0}".format(ve))
-
-    def Led_1_Off(self):
-        try:
-            mask = ~0x02;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 1 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Deactivation ValueError : {0}".format(ve))
-
-
-    def Led_1_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressLED1,level);
-        except IOError as io:
-            print ("Led 1 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 1 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 1 Brightness ValueError : {0}".format(ve))
-
-    def Led_2_On(self):
-        try:
-            mask = 0x08;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 2 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 2 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 2 Activation ValueError : {0}".format(ve))
-
-
-    def Led_2_Off(self):
-        try:
-            mask = ~0x08;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 2 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 2 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 2 Deactivation ValueError : {0}".format(ve))
-
-    def Led_2_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressLED2,level);
-        except IOError as io:
-            print ("Led 2 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 2 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 2 Brightness ValueError : {0}".format(ve))
-
-    def Led_3_On(self):
-        try:
-            mask = 0x10;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 3 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 3 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 3 Activation ValueError : {0}".format(ve))
-
-    def Led_3_Off(self):
-        try:
-            mask = ~0x10;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 3 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 3 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 3 Deactivation ValueError : {0}".format(ve))
-
-    def Led_3_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressLED3,level);
-        except IOError as io:
-            print ("Led 3 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 3 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 3 Brightness ValueError : {0}".format(ve))
-
-    def Led_4_On(self):
-        try:
-            mask = 0x40;
-            result = self.readState(self.AddressControl) | mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 4 Activation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 4 Activation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 4 Activation ValueError : {0}".format(ve))
-
-
-    def Led_4_Off(self):
-        try:
-            mask = ~0x40;
-            result = self.readState(self.AddressControl) & mask;
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressControl,result);
-        except IOError as io:
-            print ("Led 4 Deactivation IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 4 Deactivation TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 4 Deactivation ValueError : {0}".format(ve))
-
-    def Led_4_Brightness(self, level):
-        try:
-            self.bus.write_byte_data(self.BrighPiAddress,self.AddressLED4,level);
-        except IOError as io:
-            print ("Led 4 Brightness IOError : {0}".format(io))
-        except TypeError as te:
-            print ("Led 4 Brightness TypeError : {0}".format(te))
-        except ValueError as ve:
-            print ("Led 4 Brightness ValueError : {0}".format(ve))
 
     def Led_All_Brightness(self, level):
         try:
