821fbcc35106d484cd7a76211d2f9a664ed3df90
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 16:40:02 +0100

    small fix

--- led/full_ir.py
+++ led/full_ir.py
@@ -7,7 +7,7 @@ try:
     light = led.BrightPI(1)
     light.Reset();
     for times in range(100):
-        light.blink(light.IR1, 1)
+        light.Blink(light.IR1, 1)
 except IOError as io:
     print ("I/O Error ({0})".format(io));
     print " ex: sudo python demo.py"
