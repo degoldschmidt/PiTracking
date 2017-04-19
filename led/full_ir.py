9d2da4fda0cbe2e7d04abd04b969431d17d989f3
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 17:26:11 +0100

    blink test

--- led/full_ir.py
+++ led/full_ir.py
@@ -6,8 +6,12 @@ print "Full IR LED Demo by Dennis Goldschmidt"
 try:
     light = led.BrightPI(1)
     light.Reset();
-    for times in range(100):
-        light.Blink(light.IR1, 1)
+
+    time.sleep(0.5);
+    light.On(light.IR1);
+    time.sleep(0.5);
+    light.Off(light.IR1);
+    
 except IOError as io:
     print ("I/O Error ({0})".format(io));
     print " ex: sudo python demo.py"
