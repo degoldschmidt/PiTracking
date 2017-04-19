954716821319816a3d893f6cd787a14cd7f7075f
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 17:29:58 +0100

    fix

--- led/full_ir.py
+++ led/full_ir.py
@@ -3,6 +3,12 @@ import time
 
 print "Full IR LED Demo by Dennis Goldschmidt"
 
+def blink(_led):
+    time.sleep(0.5)
+    _led.On(_led.IR1)
+    time.sleep(0.5)
+    _led.Off(_led.IR1)
+
 try:
     light = led.BrightPI(1)
     light.Reset();
@@ -13,10 +19,3 @@ try:
 except IOError as io:
     print ("I/O Error ({0})".format(io));
     print " ex: sudo python demo.py"
-
-
-def blink(_led):
-    time.sleep(0.5);
-    _led.On(_led.IR1);
-    time.sleep(0.5);
-    _led.Off(_led.IR1);
