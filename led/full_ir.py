ac7d35a00b0aaa59f9f4f592700cda7f0f768332
degoldschmidt <goldschmidt.de@gmail.com>
2017-04-19 17:29:10 +0100

    blink

--- led/full_ir.py
+++ led/full_ir.py
@@ -7,11 +7,16 @@ try:
     light = led.BrightPI(1)
     light.Reset();
 
-    time.sleep(0.5);
-    light.On(light.IR1);
-    time.sleep(0.5);
-    light.Off(light.IR1);
-    
+    for numbers in range(5):
+        blink(light)
+
 except IOError as io:
     print ("I/O Error ({0})".format(io));
     print " ex: sudo python demo.py"
+
+
+def blink(_led):
+    time.sleep(0.5);
+    _led.On(_led.IR1);
+    time.sleep(0.5);
+    _led.Off(_led.IR1);
